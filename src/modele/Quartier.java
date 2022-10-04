96aa251810be633b1b563e118b460dcb0a44894c
wiwidu38 <61734178+wiwidu38@users.noreply.github.com>
2022-10-04 09:50:20 +0200

    add attributs

--- src/modele/Quartier.java
+++ src/modele/Quartier.java
@@ -1,5 +1,10 @@
 package modele;
 
 public class Quartier {
-
+	private String nom;
+	private String type;
+	private int coutConstruction;
+	private String caracteristiques;
+	public static final String TYPE_QUARTIERS;
+	
 }
