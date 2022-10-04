3593ff8e5861ef40948371d92d1a15035beb3a54
wiwidu38 <61734178+wiwidu38@users.noreply.github.com>
2022-10-04 10:12:29 +0200

    Update Quartier.java

--- src/modele/Quartier.java
+++ src/modele/Quartier.java
@@ -1,10 +1,48 @@
 package modele;
 
+import java.util.Arrays;
+
 public class Quartier {
+	public static final String[] TYPE_QUARTIERS = {"RELIGIEUX", "MILITAIRE", "NOBLE", "COMMERCANT", "MERVEILLE"};
 	private String nom;
 	private String type;
 	private int coutConstruction;
 	private String caracteristiques;
-	public static final String TYPE_QUARTIERS;
+	
+	public Quartier(String nom, String type, int cout, String caracteristiques) {
+		super();
+		setNom(nom);
+		setType(type);
+		setCout(cout);
+		setCaracteristiques(caracteristiques);
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = Arrays.asList(TYPE_QUARTIERS).contains(type) ? type  : "";
+	}
+	
+	public int getCout() {
+		return coutConstruction;
+	}
+	public void setCout(int cout) {
+		this.coutConstruction = (cout >= 1 && cout <= 6) ? cout : 0;
+	}
+	
+	public String getCaracteristiques() {
+		return caracteristiques;
+	}
+	public void setCaracteristiques(String caracteristiques) {
+		this.caracteristiques = caracteristiques;
+	}
 	
 }
