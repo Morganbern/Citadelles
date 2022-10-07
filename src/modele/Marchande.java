47990227503e249894ca9dadd62c9474315c0b99
wiwidu38 <61734178+wiwidu38@users.noreply.github.com>
2022-10-07 13:16:52 +0200

    add Marchande class with tests

---
+++ src/modele/Marchande.java
@@ -0,0 +1,26 @@
+package modele;
+
+
+public class Marchande extends Personnage{
+    
+    
+    public Marchande() {
+        super("Marchande", 6, Caracteristiques.MARCHANDE);
+    }
+    
+    public void percevoirRessourcesSpecifiques() {
+    	int nbBatCom= 0;
+    	for(int i=0; i< this.getJoueur().nbQuartiersDansCite(); i++) {
+    		if(this.getJoueur().getCite()[i].getType().equals("COMMERCANT"))
+    			nbBatCom++;
+    	}
+    	this.getJoueur().ajouterPieces(nbBatCom);
+		System.out.println("Vous avez " + nbBatCom + " batiment(s) commercants. Vous recevez donc "+ nbBatCom +" piece(s) d'or.");
+    }
+
+	public void utiliserPouvoir() {
+		System.out.println("Vous utiliser votre pouvoir. Vous recevez donc 1 piece d'or.");
+		this.getJoueur().ajouterPieces(1);
+	} 
+    
+}
